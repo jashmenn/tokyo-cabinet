9c080bdd9389dd7cba7d2b67e0bf9effed113754
Nate Murray <nate@natemurray.com>
2011-10-18 14:06:15 -0700

    changed the deps

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
-(defproject org.clojars.db/tokyo-cabinet "0.1.0"
+(defproject org.clojars.jashmenn/tokyo-cabinet "0.1.1"
   :description "tokyo cabinet client in clojure"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
-  :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]])
+  :dependencies [ [org.clojure/clojure "1.2.0"]
+                  [org.clojure/clojure-contrib "1.2.0"] 
+                  [org.clojars.nmurray/tokyocabinet "1.22-osx"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :jvm-opts ["-Xmx1400m" ~(str "-Djava.library.path=/usr/local/lib:" (System/getProperty "user.home") "/usr/lib")] 
+  )
