2fae2d915bab6b92712a903769f8d08820a577d6
Nate Murray <nate@natemurray.com>
2011-10-18 14:06:42 -0700

    put everything a function so its actually runnable with lein

--- example/test.clj
+++ example/test.clj
@@ -1,23 +1,26 @@
 
-(ns user (:use tokyo-cabinet))  ;; bring into our namespace
+(ns user 
+  (:use tokyo-cabinet))  ;; bring into our namespace
 
-(with-cabinet { :filename "test.tokyo" :mode (+ OWRITER OCREAT) } 
-  (doseq [[name val] [["1" "one"] ["2" "two"] ["3" "three"]]]
-    (put-value name val)))
+(defn -main [& args]
 
-(with-cabinet { :filename "test.tokyo" :mode OREADER }
-  (println (primary-keys)))
+  (with-cabinet { :filename "test.tokyo" :mode (+ OWRITER OCREAT) } 
+    (doseq [[name val] [["1" "one"] ["2" "two"] ["3" "three"]]]
+      (put-value-async name val))
+    (synchronize))
+
+  (with-cabinet { :filename "test.tokyo" :mode OREADER }
+    (println (primary-keys)))
 
 
+  ;; tables
 
-;; tables
+  (def params { :filename "test-table.tokyo" :mode (+ OWRITER OCREAT) :type :table } )
+  (with-cabinet params
+    (put-value "foo/x" { :name "John Doe" :hobbies "rowing fishing skiing" :age 28 :gender "M" })
+    (put-value "bar/x" { :name "Melissa Swift" :hobbies "soccer tennis books" :age 33 :gender "F"})
+    (put-value "c" { :name "Tom Swift" :hobbies "inventing exploring" :gender "M" })
+    (put-value "d" { :name "Harry Potter" :hobbies "magic quidditch flying" :gender "M" :age 9 }))
 
-(def params { :filename "test-table.tokyo" :mode (+ OWRITER OCREAT) :type :table } )
-(with-cabinet params
-  (put-value "foo/x" { :name "John Doe" :hobbies "rowing fishing skiing" :age 28 :gender "M" })
-  (put-value "bar/x" { :name "Melissa Swift" :hobbies "soccer tennis books" :age 33 :gender "F"})
-  (put-value "c" { :name "Tom Swift" :hobbies "inventing exploring" :gender "M" })
-  (put-value "d" { :name "Harry Potter" :hobbies "magic quidditch flying" :gender "M" :age 9 }))
-
-(with-cabinet params
-  (println (get-value "a/p")))
+  (with-cabinet params
+    (println (get-value "a/p"))))
